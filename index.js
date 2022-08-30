13f7bf123ef700be64b50ac80a7f1e38bc1582d0
hulk-higakijin <hm385.chejptks@gmail.com>
2022-08-30 13:24:21 +0900

    fix: 一時的にcorsを全て許可

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const http = require('http')
 const server = http.createServer(app)
 const { Server } = require("socket.io")
 const io = new Server(server, {
-  cors: { origin: ["http://localhost:3000", "https://for-plapo-apps-client.vercel.app"] }
+  // cors: { origin: ["http://localhost:3000", "https://for-plapo-apps-client.vercel.app"] }
+  cors: { origin: "*" }
 })
 
 let lists = []
